f5989f122f6a437315dc525bd67d1d706d53ce3a
gustavo <gu.nunesx@gmail.com>
2025-04-24 15:10:41 -0300

    updateGus

--- atv_turma/src/main/java/com/example/atv_turma/Controller/AlunoController.java
+++ atv_turma/src/main/java/com/example/atv_turma/Controller/AlunoController.java
@@ -1,2 +1,72 @@
-package com.example.atv_turma.Controller;public class AlunoController {
+package com.example.atv_turma.Controller;
+
+import com.example.atv_turma.Dto.AlunoDto;
+import com.example.atv_turma.Entity.Aluno;
+import com.example.atv_turma.Service.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/aluno")
+public class AlunoController {
+
+    @Autowired
+    private AlunoService alunoService;
+
+    @GetMapping
+    public List<Aluno> getAll(@RequestParam(required = false)String cpf)
+    {
+         if (cpf != null && !cpf.isEmpty()) {
+            return alunoService.getByCpf(cpf);
+        }
+
+        return alunoService.getAll();
+    }
+
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AlunoDto> getById(@PathVariable Long id){
+        Optional<AlunoDto> alunoDtoOptional = alunoService.getById(id);
+        if(alunoDtoOptional.isPresent()){
+            return ResponseEntity.ok(alunoDtoOptional.get());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<AlunoDto> create(@RequestBody AlunoDto alunoDto){
+        AlunoDto alunoDto1 = alunoService.create(alunoDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(alunoDto1);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AlunoDto> update(@PathVariable Long id, @RequestBody AlunoDto alunoDto){
+        Optional<AlunoDto> alunoDtoOptional = alunoService.updateAluno(id, alunoDto);
+        if(alunoDtoOptional.isPresent()){
+            return ResponseEntity.ok(alunoDtoOptional.get());
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        if(alunoService.delete(id)){
+            return ResponseEntity.noContent().build();
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+
+
+
 }
--- atv_turma/src/main/java/com/example/atv_turma/Controller/ProfessorController.java
+++ atv_turma/src/main/java/com/example/atv_turma/Controller/ProfessorController.java
@@ -1,4 +1,71 @@
 package com.example.atv_turma.Controller;
 
+
+import com.example.atv_turma.Dto.ProfessorDto;
+import com.example.atv_turma.Entity.Aluno;
+import com.example.atv_turma.Entity.Professor;
+import com.example.atv_turma.Service.ProfessorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/professor")
 public class ProfessorController {
+
+    @Autowired
+    private ProfessorService professorService;
+
+    @GetMapping
+    public List<Aluno> getAll(@RequestParam(required = false)String nome)
+    {
+        if (nome != null && !nome.isEmpty()) {
+            return professorService.getByNome(nome);
+        }
+
+        return professorService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProfessorDto> getById(@PathVariable Long id){
+        Optional<ProfessorDto> professorDtoOptional = professorService.getById(id);
+        if(professorDtoOptional.isPresent()){
+            return ResponseEntity.ok(professorDtoOptional.get());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<ProfessorDto> create(@RequestBody ProfessorDto professorDto){
+        ProfessorDto professorDtoSave  = professorService.create(professorDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(professorDtoSave);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProfessorDto> update(@PathVariable Long id, @RequestBody ProfessorDto professorDto){
+        Optional<ProfessorDto> professorDtoOptional = professorService.up(id, professorDto);
+        if(professorDtoOptional.isPresent()){
+            return ResponseEntity.ok(professorDtoOptional.get());
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        if(professorService.delete(id)){
+            return ResponseEntity.noContent().build();
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+
+
 }
