2247d25ece3237e0be72d859980b52870aac4b05
gustavo <gu.nunesx@gmail.com>
2025-04-24 14:16:05 -0300

    gustavo

---
+++ atv_turma/src/main/java/com/example/atv_turma/AtvTurmaApplication.java
@@ -0,0 +1,13 @@
+package com.example.atv_turma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtvTurmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtvTurmaApplication.class, args);
+	}
+
+}
---
+++ atv_turma/src/test/java/com/example/atv_turma/AtvTurmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atv_turma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtvTurmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
