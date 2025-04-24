8d19bc25b401e9cf81652a6f00fcfa7f4d5c4437
gustavo <gu.nunesx@gmail.com>
2025-04-24 16:02:24 -0300

    uodate gus

--- atv_turma/src/main/java/com/example/atv_turma/Entity/Turma.java
+++ atv_turma/src/main/java/com/example/atv_turma/Entity/Turma.java
@@ -24,11 +24,6 @@ public class Turma implements Serializable {
     @JoinColumn(name = "id_professor", referencedColumnName = "id")
     private Professor professor;
 
-    //@OneToMany mudar
-    @JoinTable(
-            name = "turma_aluno",
-            joinColumns = @JoinColumn(name = "turma_id"),
-            inverseJoinColumns = @JoinColumn(name = "aluno_id")
-    )
+   
     private List<Aluno> alunos;
 }
